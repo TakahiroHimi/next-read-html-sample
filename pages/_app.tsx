5cbef489f9ecf8cc5810f2e16aeca3bbbe6b02f8
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-06-23 13:36:24 +0900

    fix: 動的ルーティング対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import paths from "../paths";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {paths.map((path) => (
+        <>
+          <Link href={path}>
+            <a>{path}</a>
+          </Link>
+          <br />
+        </>
+      ))}
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
---
+++ paths.ts
@@ -0,0 +1,3 @@
+const paths = ["/", "onchan", "ichimoni", "ichi"];
+
+export default paths;
