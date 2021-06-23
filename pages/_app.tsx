9b5778308b19bbbd7b9e51d877d8f765b695802f
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-06-23 16:11:07 +0900

    update: globalメニューのリンクを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,132 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
-import paths from "../paths";
+import {
+  announcers,
+  event,
+  goods,
+  hokkaido,
+  movie,
+  news,
+  onchan,
+  paths,
+  program,
+  sports,
+} from "../paths";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {paths.map((path) => (
-        <>
-          <Link href={path}>
-            <a>{path}</a>
-          </Link>
-          <br />
-        </>
-      ))}
+      <div style={{ display: "flex" }}>
+        <div>
+          <p>番組情報</p>
+          <ul>
+            {program.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>ニュース・天気</p>
+          <ul>
+            {news.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>スポーツ</p>
+          <ul>
+            {sports.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>アナウンサー</p>
+          <ul>
+            {announcers.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>onちゃん</p>
+          <ul>
+            {onchan.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>グッズ</p>
+          <ul>
+            {goods.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>イベント</p>
+          <ul>
+            {event.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>動画・SNS</p>
+          <ul>
+            {movie.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>北海道</p>
+          <ul>
+            {hokkaido.map((path) => (
+              <Link key={path} href={path}>
+                <a>
+                  <li>{path}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
       <Component {...pageProps} />
     </>
   );
--- paths.ts
+++ paths.ts
@@ -1,3 +1,120 @@
-const paths = ["/", "onchan", "ichimoni", "ichi"];
+export const paths = ["/", "onchan", "ichimoni", "ichi"];
 
-export default paths;
+export const program = [
+  "lineup",
+  "bangumi",
+  "chimata",
+  "biz",
+  "hanatare/golden2",
+  "ichimoni/sunagawa",
+  "kirakira",
+  "channel",
+  "tv",
+  "kaitaku",
+  "ichimoni",
+  "ichi",
+  "hit/night/index.html",
+  "lovehokkaido",
+  "med",
+  "hybridcast/med",
+  "gogokoureisya",
+  "kisei_2021",
+  "gakushitsu",
+  "onigiri",
+  "suidou",
+  "bangumi/log/20160628_120234.html",
+  "hanatare",
+  "neochi",
+  "yumechika",
+  "fffff",
+  "nmb",
+];
+
+export const news = [
+  "news",
+  "news/senkyo",
+  "telemen",
+  "telemen/poneohau",
+  "telemen/special2020",
+  "telemen/selection2020",
+  "telemen/nuclear",
+  "telemen/kaigo",
+  "news/inochi/index.html",
+  "telemen/tancho",
+  "telemen/pinkribbon",
+  "telemen/ainu",
+  "news/oiru",
+  "sagi",
+  "mikio_journal",
+  "selections",
+  "news/tsutaenokosu",
+  "hn",
+  "hn/log/2021/03141110",
+  "hn/log/2020/12281114",
+  "hn/log/2020/06271156",
+  "hn/log/2020/05091659",
+  "weather",
+  "news/info.html",
+  "disaster",
+];
+
+export const sports = ["sports", "fffff"];
+
+export const announcers = [
+  "announcers",
+  "announcers/anaonpaper",
+  "announcers/anakouza",
+  "announcers/ohanashi",
+  "dekirukoto/announcers",
+  "announcers/sairo",
+  "",
+];
+
+export const onchan = [
+  "onchan",
+  "onapp",
+  "onchan/blog",
+  "onchan/park",
+  "chimata",
+  "onchan/campuslife",
+  "oncookie",
+  "onchan/tsumesyougi",
+  "onanime",
+  "ebooks",
+];
+
+export const goods = ["goods", "yumechika/records"];
+
+export const event = [
+  "event/online_theater",
+  "event/tamiya",
+  "event/thedoraemontensapporo2021",
+  "event/ghibli",
+  "event/sanagi",
+  "event",
+  "movie",
+  "event/axa_online",
+  "event/htbyose_05",
+  "caravan2020",
+  "event/snowfes/2020/index.html",
+  "event/htbcup",
+  "parkair",
+  "matsuri",
+  "fesinsapporo2019",
+];
+
+export const movie = ["hodinfo", "dougatoukou", "sns"];
+
+export const hokkaido = [
+  "shikanohi",
+  "datacast",
+  "event/lab",
+  "datacast/ontoku",
+  "hakarikko",
+  "8020",
+  "yellow",
+  "ipedia",
+  "ikinari",
+  "kanda",
+  "fox",
+];
